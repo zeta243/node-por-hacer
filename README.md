cfa38ed6f7a275cb6167634bce4c93788a6bd458
Jhon <zeta243@gmail.com>
2020-01-29 11:52:38 -0500

    Primer commir

---
+++ README.md
@@ -0,0 +1,7 @@
+## Aplicación de comandos
+
+Este es el ejercicio
+Recuerden instalar los paquetes de node 
+----
+npm install 
+----
---
+++ config/yargs.js
@@ -0,0 +1,34 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer'
+};
+
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completado o pendiente la tarea'
+};
+
+
+
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borrar una tarea', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+
+
+
+module.exports = {
+    argv
+}
